31ba4ea7f5d20a93d1a0c494ba5eae1aa4494487
pedropassos386-source <pedropassos386@gmail.com>
2026-06-16 15:39:57 -0300

    add o perfil e o footer

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,20 @@ document.getElementById("submenu");
 btn.addEventListener("click", () => {
     menu.classList.toggle("ativo");
 })
+
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 1500){
+        btnTopo.classList.add("mostrar");
+    }else{
+        btnTopo.classList.remove("mostrar");
+    }
+});
+
+btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
